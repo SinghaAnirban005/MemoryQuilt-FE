71c0b3ab5bca5b1a2c08236c29f95b74ff6f015e
SinghaAnirban005 <anirbansingha20@gmail.com>
2025-06-16 03:11:29 +0530

    frontend: Refactor Home page

--- memory-fe/src/icons/AddIcon.tsx
+++ memory-fe/src/icons/AddIcon.tsx
@@ -1,5 +1,6 @@
 interface AddIconProps {
-  size : "lg" | "md" | "sm"
+  size : "lg" | "md" | "sm",
+  className?: string
 }
 
 const sizeVariants = {
@@ -8,8 +9,8 @@ const sizeVariants = {
   "lg": "size-6"
 }
 
-export function AddIcon({size} : AddIconProps) {
-    return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={sizeVariants[size]}>
+export function AddIcon({size, className} : AddIconProps) {
+    return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={sizeVariants[size] = ` ${className}}`}>
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 </svg>
 }
